db2ae1cd4870382e2076fb213f93a040fb32934d
Orly Neto <orly.neto@proton.me>
2026-06-20 22:55:34 -0300

    Opening, creating and modifying existing storage/data/user files feature; Save ft.TextButton added to editor_view()'s appbar; File list built with ft.Column in home_view() replaced for a ft.ListView with ft.Row(s) inside.

--- src/main.py
+++ src/main.py
@@ -4,9 +4,11 @@ from pathlib import Path
 
 APP_DATA_PATH: Path = Path(os.getenv("FLET_APP_STORAGE_DATA"))
 APP_DATA_TEMP_PATH: Path = Path(os.getenv("FLET_APP_STORAGE_TEMP"))
+USER_DIR: Path = APP_DATA_PATH / "user"
 
 APP_DATA_PATH.mkdir(parents=True, exist_ok=True)
 APP_DATA_TEMP_PATH.mkdir(parents=True, exist_ok=True)
+USER_DIR.mkdir(parents=True, exist_ok=True)
 
 async def main(page: ft.Page):
     page.padding = 4
@@ -16,17 +18,21 @@ async def main(page: ft.Page):
         async def goto_editor():
             await page.push_route("/editor")
         async def new_file(e):
-            page.session.store.set("textfield_default_value", "")
+            page.session.store.set("textbox", "")
+            page.session.store.set("title", "")
+            page.session.store.set("path", None)
             await goto_editor()
         async def open_file(e):
-            file_path: Path = e.control.data
+            file_path: Path = Path(e.control.data)
             with open(file_path, "r") as f:
                 content: str = f.read()
-            page.session.store.set("textfield_default_value", content)
+            page.session.store.set("path", file_path)
+            page.session.store.set("textbox", content)
+            page.session.store.set("title", file_path.stem)     
             await goto_editor()
 
         def user_files():
-            path = APP_DATA_PATH / "user"
+            path: Path = USER_DIR
             return [file for file in path.glob("*")]
 
         appBar = ft.AppBar(
@@ -42,12 +48,16 @@ async def main(page: ft.Page):
             route="/",
             controls=[
                 ft.SafeArea(
-                    content=ft.Column(
+                    content=ft.ListView(
                         controls=[
-                            ft.TextButton(
-                                file.name,
-                                data=file.resolve(),
-                                on_click=open_file
+                            ft.Row(
+                                controls=[
+                                    ft.TextButton(
+                                        file.name,
+                                        data=file.resolve(),
+                                        on_click=open_file
+                                    )
+                                ]
                             ) for file in user_files()
                         ]
                     )
@@ -58,10 +68,51 @@ async def main(page: ft.Page):
         return view
 
     def editor_view() -> ft.View:
-        def default_value() -> str:
-            return page.session.store.get("textfield_default_value")
+        def default_value(key: str) -> str:
+            return page.session.store.get(key)
+        async def save_file(e):
+            file_title: str = title_field.value
 
-        app_bar = ft.AppBar()
+            if not file_title:
+                file_title = "Untitled File"
+
+            file_content: str = textbox_field.value
+            print(f"{title_field.value=}")
+            print(f"{textbox_field.value=}")
+
+            path: Path = USER_DIR / f"{file_title}.txt"
+            old_path: Path | None = page.session.store.get("path")
+            print(f"{old_path=}")
+            with open(path, "w") as f:
+                f.write(file_content)
+
+            if old_path != None and old_path.exists():
+                os.replace(old_path, path)
+
+        title_field = ft.TextField(
+            expand=True,
+            border=ft.InputBorder.NONE,
+            hint_text="Title",
+            value=default_value("title"),
+        )
+
+        textbox_field = ft.TextField(
+            expand=True,
+            border=ft.InputBorder.NONE,
+            text_size=16,
+            multiline=True,
+            hint_text="...",
+            value=default_value("textbox"),
+        )
+
+        app_bar = ft.AppBar(
+            actions=[
+                ft.TextButton(
+                    "Save",
+                    on_click=save_file,
+                )
+            ]
+        )
         view = ft.View(
             route="/editor",
             controls=[
@@ -72,21 +123,11 @@ async def main(page: ft.Page):
                         controls=[
                             ft.Row(
                                 controls=[
-                                    ft.TextField(
-                                        expand=True,
-                                        border=ft.InputBorder.NONE,
-                                        hint_text="Title",
-                                    ),
+                                    title_field,
+                                    #textbox_field,
                                 ],
                             ),
-                            ft.TextField(
-                                expand=True,
-                                border=ft.InputBorder.NONE,
-                                text_size=16,
-                                multiline=True,
-                                hint_text="...",
-                                value=default_value()
-                            )
+                            textbox_field,
                         ]
                     )
                 )
